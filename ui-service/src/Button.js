890ed89196260bbb2b795639640c069436864642
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2025-06-05 06:09:50 +0500

    everything worked perfectly:

--- ui-service/src/Button.js
+++
@@ -1,9 +0,0 @@
-function Button({className, onClick}){
-    return(
-        <div>
-            <button className={className} onClick={onClick}>Hello it finally worked</button>
-        </div>
-    );
-}
-
-export default Button;
